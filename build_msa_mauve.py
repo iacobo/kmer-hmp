81521a01291c1f7b5606025ea653ff90e7959a29
Jacob <47821203+ja-ox@users.noreply.github.com>
2019-06-26 14:11:56 +0100

    Changed path handling.

--- build_msa_mauve.py
+++ build_msa_mauve.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Script designed to:
-    
     1. Reorder contigs for multiple draft files against a single reference file 
        using Mauve on Windows.
     2. Align reordered sequences with reference file using Mauve.
@@ -13,12 +11,11 @@ See Mauve documentation for further info:
 """
 
 import os
-import glob
 import subprocess
 import datetime
+from pathlib import Path
 
-from Bio import AlignIO
-from Bio import Entrez
+from Bio import AlignIO, Entrez
 
 def order_contigs(reference, draft, output_dir, mauve_dir, java_dir='java'):
     """Call Mauve to reorder the contigs of a draft sequence relative to
@@ -32,7 +29,7 @@ def align_seqs(reference, sequences, output='alignment.xmfa'):
     """Align [multiple] sequences to a reference sequence using Mauve.
     """
     command = f'progressiveMauve --output={output} {reference} '
-    command += ' '.join(sequences)
+    command += ' '.join(map(str, sequences))
     subprocess.call(command)
     
 def grab_latest_alignment(suffixes, results_dir):
@@ -42,15 +39,15 @@ def grab_latest_alignment(suffixes, results_dir):
     the most recent.
     
     i.e.
-    C:\\Users\\User\\Documents\\alignment_sequence_X\\alignment1
-    C:\\Users\\User\\Documents\\alignment_sequence_X\\alignment2
-    C:\\Users\\User\\Documents\\alignment_sequence_X\\alignment3   <<< 
+    C:/Users/User/Documents/alignment_sequence_X/alignment1
+    C:/Users/User/Documents/alignment_sequence_X/alignment2
+    C:/Users/User/Documents/alignment_sequence_X/alignment3   <<< 
     """
     seqs = []
     for suffix in suffixes:
-        # WARNING: Max logic will fail if more than 9 files (i.e. '10' < '2', lexicographically)
+        # WARNING: Max logic will fail if more than 9 files (i.e. '10' < '2' lexicographically)
         try:
-            file = max(glob.glob(f'{results_dir}alignment_{suffix}\\alignment*\\{suffix}*.fas'))
+            file = max(results_dir.glob(f'alignment_{suffix}/alignment*/{suffix}*.fas'))
             seqs.append(file)
         except FileNotFoundError as fnf_error:
             print(f'Error: {fnf_error}')
@@ -64,7 +61,7 @@ def grab_fasta_files(location):
     extensions = ['fasta','fa','faa','fna','ffn','frn']
     files = []
     for extension in extensions:
-        files.extend(glob.glob(f'{location}*.{extension}'))
+        files.extend(location.glob(f'*.{extension}'))
     return files
 
 def download_genome(search_term, directory, filetype='fasta', email=None):
@@ -81,7 +78,7 @@ def download_genome(search_term, directory, filetype='fasta', email=None):
     genome_id = Entrez.read(handle)['IdList'][0]
     record = Entrez.efetch(db='nucleotide', id=genome_id, rettype=filetype, retmode='text')
     
-    filename = f'{directory}Record_{genome_id}.{filetype}'
+    filename = directory / f'Record_{genome_id}.{filetype}'
     
     print(f'Writing: {filename}')
     with open(filename, 'w') as f:
@@ -101,40 +98,32 @@ def sort_multi_alignment_by_reference(multialignment, reference='reference'):
     ## DON'T ASSUME REFERENCE IS FIRST RECORD
     multialignment.sort(key = lambda x: (all([reference not in record.id for record in x]), int(x[0].id.split('-')[-1])))
 
-
-####################################
-## Main program
-####################################
     
-def main(base_path=None, reference=None, reorder=False, mauve_dir=None):
+def main(base_path=Path(''), reference=None, reorder=False, mauve_dir=Path('')):
     
-    ############################################
-    ## Sequence files
-    ############################################
-    
-    # Download reference genome
+    # Search criteria to download reference genome
     searches = ['staphylococcus[orgn]','MSSA476 complete genome[title]','NC_002953.3[accession]']
     search_term = ' AND '.join(searches)
-    output_dir = f'{base_path}reference_genome'
+    
+    output_dir = base_path / 'reference_genome'
     
     # Download reference file if not specified
-    if not reference:
+    if reference is None:
         reference = download_genome(search_term, output_dir)
 
     # Locations of executables to run in cmd
-    # Note: double quotes necessary for whitespace in directory names
-    java_dir = '"C:\\Program Files (x86)\\Common Files\\Oracle\\Java\\javapath_target_10534109\\java.exe"'
-    mauve_dir = '"C:\\Program Files (x86)\\Mauve 20150226\\Mauve.jar"'
+    java = Path('C:/Program Files (x86)/Common Files/Oracle/Java/javapath_target_10534109/java.exe')
+    mauve = Path('C:/Program Files (x86)/Mauve 20150226/Mauve.jar')
     
-    # Seq files of which to reorder contigs
-    drafts_dir = f'{base_path}fa_files\\'
-    results_dir = f'{base_path}ordered_contigs\\'
-    alignment_filename = f'{base_path}alignment\\alignment.xmfa'
-
+    # MSA file to reorder contigs of
+    drafts_dir = base_path / 'fa_files'
+    results_dir = base_path / 'ordered_contigs'
+    alignment_filename = base_path / 'alignment/alignment.xmfa'
+    
+    # Reordering genomes relative to reference
     if reorder:
-        if mauve_dir:
-            # Change directory to execute progressiveMauve (necessary if not in PATH)
-            os.chdir(mauve_dir)
+        # Change directory to execute progressiveMauve (necessary if not in PATH)
+        os.chdir(mauve_dir)
         # Container for draft file names
         draft_filenames = []
     
@@ -142,19 +131,21 @@ def main(base_path=None, reference=None, reorder=False, mauve_dir=None):
         t0 = datetime.datetime.now() 
         # Loops through files in directory of '.fa' format
         for draft in grab_fasta_files(drafts_dir):
-            draft_filename = draft.split('\\')[-1].split('.')[-2]
+            # i.e. selects name from directory/stem.extension
+            draft_filename = Path(draft).stem
             draft_filenames.append(draft_filename)
-            output_dir = f'{results_dir}alignment_{draft_filename}'
+            output_dir = results_dir / f'alignment_{draft_filename}'
             
             # Orders contigs of each draft sequence relative to reference
-            order_contigs(reference, draft, output_dir, mauve_dir, java_dir)
+            order_contigs(reference, draft, output_dir, mauve, java)
             
         t1 = datetime.datetime.now()
         print(f'Total time: {(t1-t0).total_seconds():.2f}s.\n')
         
+        # Creating Multi-Sequence_Alignment
         print('Aligning sequences...')
         t0 = datetime.datetime.now() 
-        # Aligns all draft sequences relative to reference
+        # Align draft sequences relative to reference
         seqs = grab_latest_alignment(draft_filenames, results_dir)
         align_seqs(reference, seqs, alignment_filename)
         t1 = datetime.datetime.now()
@@ -164,6 +155,7 @@ def main(base_path=None, reference=None, reorder=False, mauve_dir=None):
     print('Loading alignment file...')
     t0 = datetime.datetime.now() 
     alignments = list(AlignIO.parse(alignment_filename, 'mauve'))
+    
     # Sort alignments to match ordering of reference genome
     sort_multi_alignment_by_reference(alignments, reference='reference')
     t1 = datetime.datetime.now()
@@ -172,9 +164,9 @@ def main(base_path=None, reference=None, reorder=False, mauve_dir=None):
     return alignments
 
 if __name__ == '__main__':
-    mauve_dir = 'C:\\Program Files (x86)\\Mauve 20150226'
-    base_path = 'C:\\Users\\Jacob\\Downloads\\fusidic_data\\'
-    reference = f'{base_path}reference_genome\\MSSA476.fasta'
+    mauve_dir = Path('C:/Program Files (x86)/Mauve 20150226')
+    base_path = Path('C:/Users/Jacob/Downloads/fusidic_data')
+    reference = base_path / 'reference_genome/MSSA476.fasta'
     
     os.chdir(base_path)
     main(base_path=base_path, reference=reference, mauve_dir=mauve_dir)
