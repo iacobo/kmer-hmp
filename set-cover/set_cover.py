4d91f37cc6a5c6c73feffc76ad6e063087e21389
Jacob <47821203+ja-ox@users.noreply.github.com>
2019-06-27 11:34:52 +0100

    Update set_cover.py

--- set-cover/set_cover.py
+++ set-cover/set_cover.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Script to:
-    
     1. Determine smallest subset of genomes required to encapsulate >= x% of all
        k-mers present in complete genome set.
        
@@ -10,11 +8,11 @@ Effectively a set covering problem.
 Implementation: Greedy algorithm. 
                 Not guaranteed minimum, but at most factor of log(n) off.
 """
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-# TODO: Weighted version. Change DF to have n instead of 1 for each pattern.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 def sample_file(x,y):
     """Return DataFrame of size (1,y) with each element a random binary string
@@ -23,85 +21,72 @@ def sample_file(x,y):
     df = pd.DataFrame([''.join(map(str, np.random.randint(2, size=(x,)))) for _ in range(y)], columns=['bin'])
     return df
 
-if __name__ == '__main__':
-    
-    base_path = 'C:\\Users\\Jacob\\Downloads\\fusidic_data\\'
-    patterns = f'{base_path}patternindex\\cipro_all_kmer_out.patternKey.txt' 
-    pattern_index = f'{base_path}patternindex\\cipro_all_kmer_out.patternIndex.txt'
-    
-    load_file = True
-    whitespace = False
-    weighted = True
-    
+def len_first_line(file):
+    """Return length of first line of file.
+    """
+    with open(file) as f:
+        length = len(f.readline().strip())
+    return length
+
+def plot_curve(kmer_cumcounts, n_kmers, n_genomes, proportion):
+    """Plot curve of number of genomes vs k-mer coverage.
+    """
+    plt.plot(range(len(kmer_cumcounts)), np.array(kmer_cumcounts)/n_kmers) # Remove hardcoding of length
+    plt.plot([0,len(kmer_cumcounts)], [proportion, proportion], 'r--')
+    plt.xlabel(f'Number of genomes ({n_genomes} total)')
+    plt.ylabel(f'Proportion of k-mers covered \n({n_kmers} total)')
+    plt.title('Set covering')
+    plt.suptitle('Minimum number of genomes required to capture y% of all k-mers')
+
+def main(patterns, pattern_index=None, weighted=True, proportion=0.8):
     # Load Patterns file as DataFrame
-    if load_file:
-        dft = pd.read_csv(patterns, names=['bin']) # Dataframe where columns are genome presence, rows are k-mer patterns
+    # Row = k-mer pattern
+    # Col = genome presence/absence in patterns
+    n_genomes = len_first_line(patterns)
+    df = pd.read_fwf(patterns, header=None, widths = [1]*n_genomes)
         
-        assert max(dft['bin'].apply(len)) == min(dft['bin'].apply(len)), "Not all patterns of same length!"
+    # Weighted version: pat weighted by number of k-mers sharing that pattern
+    if weighted:
+        pattern_index = pd.read_csv(pattern_index, names=['i'])
+        weights = pattern_index['i'].value_counts().sort_index()
         
-        pattern_count = len(dft)
-        genome_count = len(dft['bin'].values[0])
-        
-        # Only consider n genomes (not enough RAM to deal with all ~1000 at once, ~400 is memory limit
-        n = 450
-        start = 542 - n
-        stop = 542
-        dft['bin'] = dft['bin'].str[start:stop]
-        print('Dataframe slice taken.')
-        
-        if whitespace:
-            # Remove any trailing spaces
-            dft['bin'] = dft['bin'].str.rstrip()
-        
-        # Split string column into single-char columns
-        dft = dft['bin'].apply(lambda x: pd.Series(list(x)))
-        print('Binary string converted to columns.')
-        
-        # Convert '0', '1' to ints
-        dft = dft.apply(pd.to_numeric)
-        print('Column types converted to numeric.')
-        
-        # Weighted version
-        if weighted:
-            patinddf = pd.read_csv(pattern_index, names=['i'])
-            weights = patinddf['i'].value_counts().sort_index()
-            
-            assert len(dft) == len(weights), "Index file does not match patterns file!"
-            # Times row i by weight i  (1 > i, 0 > 0)
-            dft = dft.multiply(weights, axis=0)
-            print("Weights applied.")
-        
-        #Transpose dataframe to get columns as kmers and rows as genomes
-        dft = dft.T
+        assert len(df) == len(weights), "Index file does not match patterns file!"
+        # Times row i by weight i  (1 > i, 0 > 0)
+        df = df.multiply(weights, axis=0)
+        print("Weights applied.")
     
-    # Initialise cumulative counts at 0
-    kmer_cumcounts = [0]
-    proportion = 0.9
+    # Transpose dataframe to get:
+    # Row = genome presence/absence in patterns
+    # Col = k-mer pattern
+    df = df.T
+    print('DataFrame loaded.')
     
     if weighted:
-        total = sum(weights)
+        n_kmers = len(pattern_index)
     else:
-        total = pattern_count
+        # Actually total number of patterns, not k-mers
+        n_kmers = len(df.columns)
     
-    threshold = proportion*total
-        
+    # Minimum number to catch
+    threshold = proportion*n_kmers
+    kmer_cumcounts = [0]
     indices = []
-    
-    print('DataFrame loaded.')
     
     # Set cover - greedy algorithm
     while kmer_cumcounts[-1] < threshold:
-        sums =  dft.sum(axis=1, skipna=True)
+        sums =  df.sum(axis=1, skipna=True)
         try:
-            row = dft.loc[[sums.idxmax()]].iloc[[0]] # ONLY GRAB ONE ROW (multiple lines may have same sum)
+            # Only grab one (first) row (multiple lines may have same sum)
+            row = df.loc[[sums.idxmax()]].iloc[[0]]
             index = row.index[0]
             indices.append(index)
-            print('\n', row)
             
-            # Update DataFrame for next iteration
-            column_mask = np.logical_not(row).values[0] # Exclude columns in present row
-            row_mask = dft.index != index # Exclude present row
-            dft = dft.loc[row_mask,column_mask]
+            # Exclude non-zero columns from current row
+            column_mask = np.logical_not(row).values[0]
+            # Exclude current row
+            row_mask = df.index != index
+            # Update DataFrame for next iteration:
+            df = df.loc[row_mask,column_mask]
             
             kmer_cumcounts.append(kmer_cumcounts[-1] + sums.max())
             print(f'Number of k-mers covered: {kmer_cumcounts[-1]}')
@@ -109,17 +94,17 @@ if __name__ == '__main__':
             print("No further novel k-mers present in this iteration.")
             break
     
-    # Give absolute ot relative index values
-    indices = np.array(indices) + start
-    
-    print('-----')
     print(f'Covering set row numbers: {indices}')
     
-    # Plot coverage curve
-    plt.plot(range(len(kmer_cumcounts)), np.array(kmer_cumcounts)/total) # Remove hardcoding of length
-    plt.plot([0,len(kmer_cumcounts)], [proportion, proportion], 'r--')
-    plt.xlabel(f'Number of genomes ({genome_count} total)')
-    plt.ylabel(f'Proportion of k-mer (patterns) covered \n({total} total)')
-    plt.title('Set covering')
-    plt.suptitle('Minimum number of genomes required to capture y% of all k-mers')
-    plt.text(0, -0.35, f'Note: only considering {n} genomes ({start} - {stop}). \nGlobal performance may be better.')
+    # Plot curve of number of genomes vs k-mer coverage
+    plot_curve(kmer_cumcounts, n_kmers, n_genomes, proportion)
+    
+    return indices
+
+if __name__ == '__main__':
+    
+    base_path = Path('C:/Users/Jacob/Downloads/fusidic_data/static_files')
+    patterns = base_path / 'fusidic_acid_patternKey.txt' 
+    pattern_index = base_path / 'fusidic_acid_patternIndex.txt'
+    
+    main(patterns, pattern_index)
